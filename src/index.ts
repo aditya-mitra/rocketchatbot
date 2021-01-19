d0c0f02a398533bc2d7fcb7dde225c823f1a91a0
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-01-20 01:19:11 +0530

    rocket bot message

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
 import { config as dotenvConfig } from "dotenv";
+import { driver, api } from "@rocket.chat/sdk";
 
 dotenvConfig();
 
-console.log("written in tsc");
+const { ROCKETCHAT_URL, ROCKETCHAT_USER, ROCKETCHAT_PASSWORD } = process.env;
+
+async function main() {
+  await driver.connect({ host: ROCKETCHAT_URL, useSsl: false });
+
+  await driver.login({
+    username: ROCKETCHAT_USER,
+    password: ROCKETCHAT_PASSWORD || "no-password",
+  });
+
+  await driver.login({
+    username: ROCKETCHAT_USER,
+    password: ROCKETCHAT_PASSWORD || "no-password",
+  });
+
+  await driver.joinRooms(["general"]);
+
+  await driver.subscribeToMessages();
+
+  await driver.sendToRoom("this is fast hello", "general");
+}
+
+main();
